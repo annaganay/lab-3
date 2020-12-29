0729c798993984889887c468157a04e49e440e12
annaganay <71323607+annaganay@users.noreply.github.com>
2020-12-29 23:35:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
-from struct import pack
 import numpy as np
-import math
+from tkinter import *
 
-HEIGHT = 600
-WIDTH = 600
+print('Введите ширину файла(в пиксилях):')
+h = int(input())
 
-file = open('result.bmp', 'wb')
-file.write(b'BM')
-file.write(pack('<LHHL', 62 + HEIGHT*WIDTH, 0, 0, 62))
-file.write(pack('<LLLHHLLLLLL', 40, HEIGHT, WIDTH, 1, 8, 0, 0, 0, 0, 0, 0))
-color_0 = (0, 0, 0, 0)
-color_1 = (255, 255, 255, 0)
-file.write(pack('<8B', *color_0, *color_1))
+min = 600
+
+if h < min:
+    h = min
+
+root = Tk()
+
+c = Canvas(root, width=h, height=h, bg='white')
+c.pack()
+
+c.create_line(h/2, h, h/2, 0, arrow=LAST)
+c.create_line(0, h/2, h, h/2, arrow=LAST)
+
+for x in range(10):
+    c.create_line(h/2-6, x/10*h, h/2+7, x/10*h)
+    c.create_line(x/10*h, h/2-6, x/10*h, h/2+7)
+
 
 t = np.linspace(0, 4 * np.pi, 600)
-x = 2*(np.cos(2*t) - np.cos(4*t)/2)
-y = 2*(np.cos(2*t) - np.sin(4*t)/2)
+x = 2*(np.cos(2*t) - np.cos(4*t)/2)*h/10
+y = 2*(np.cos(2*t) - np.sin(4*t)/2)*h/10
 x = [round(v, 5) for v in x]
 y = [round(v, 5) for v in y]
 
-picture = []
 
-for i in range(600):
-    picture.append([x[i], y[i]])
+for i in range(599):
+    c.create_line(x[i]+h/2, -y[i]+h/2, x[i+1]+h/2, -y[i+1]+h/2, width=2, fill='red')
 y.sort()
 
-for i in y:
-    x_in_function = []
-    for pair in picture:
-        if pair[1] == i:
-            x_in_function.append(pair[0])
-    x_pixels = []
-    for cur_y in x_in_function:
-        x_pixels.append(math.trunc(cur_y*106 + 300))
-    for a in range(WIDTH):
-        if a in x_pixels:
-            file.write(pack('<B', 0))
-        else:
-            file.write(pack('<B', 1))
-file.close()
+c.update()
+c.postscript(file="picture.bmp")
+
+root.mainloop()
